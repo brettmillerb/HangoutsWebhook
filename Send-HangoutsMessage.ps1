5345d6b9f118190b58c78112956b8bf9a5fac38c
Brett Miller <brett@millerb.co.uk>
2018-03-08 14:42:13 +0000

    Added Parameters and Invoke-RestMethod rather than just outputting the raw Json

--- Send-HangoutsMessage.ps1
+++ Send-HangoutsMessage.ps1
@@ -3,9 +3,32 @@ function Send-HangoutsMessage {
 
     Param (
         [Parameter(Mandatory = $true,
+            Position = 0,
             ValueFromPipeline = $true)]
-        [object]$Payload
+        [object]$Payload,
+
+        [Parameter(Mandatory = $true,
+            Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [string]$WebhookUri,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Proxy
     )
 
     $payload | ConvertTo-Json -Depth 50
+
+    $params = @{
+        Uri         = $WebhookUri
+        Method      = 'POST'
+        ContentType = 'application/JSON'
+        Proxy       = if ($proxy) {$proxy}
+    }
+
+    try{ 
+        Invoke-RestMethod @params
+    }
+    catch {
+        Write-Error $_[0].exception.message
+    }
 }
